0c733857e8e74db4ab129c7db275467ee1bab43c
mactive <mactive@gmail.com>
2016-03-26 20:20:30 +0800

    reactive signal

--- RACBabyDoodle/ViewController.m
+++ RACBabyDoodle/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
+@property (nonatomic, strong) IBOutlet UITextField *nameField;
+@property (nonatomic, strong) IBOutlet UIButton *button;
 
 @end
 
@@ -16,8 +19,53 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  
+  
+  
+  // 绑定第一个
+//  [self.nameField.rac_textSignal subscribeNext:^(id x) {
+//    NSLog(@"%@",x);
+//  }];
+  
+  // 可以绑定两个
+  [self.nameField.rac_textSignal subscribeNext:^(id x) {
+    NSLog(@"2-%@",x);
+  } error:^(NSError *error) {
+    NSLog(@"2-%@",error);
+  } completed:^{
+    NSLog(@"2-Complete");
+  }];
+  
+  
+  [self racAction];
+  
+  
+  
   // Do any additional setup after loading the view, typically from a nib.
 }
+
+-(void)racAction{
+  
+  RACSignal * validEmailSignal = [self.nameField.rac_textSignal map:^id(id value) {
+    return @([value rangeOfString:@"@"].location != NSNotFound);
+  }];
+  
+  RAC(self.button, enabled) = validEmailSignal;
+  
+  
+  RAC(self.nameField, textColor) = [validEmailSignal map:^id(id value) {
+    if([value boolValue]){
+      return [UIColor greenColor];
+    }else{
+      return [UIColor blueColor];
+    }
+  }];
+  
+  
+  
+}
+
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
